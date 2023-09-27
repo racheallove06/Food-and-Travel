a26c415dab7c523aa67654f6302fd04139c3e1de
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-27 12:31:39 +0300

    added the js styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,26 @@
 import React from "react";
-
+import { Autocomplete } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Autocomplete>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+        </Autocomplete>
+      </Toolbar>
+    </AppBar>
+  );
 }
