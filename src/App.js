0866571c5fa4e7fd63195bb785dc44a0824551af
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-27 09:59:16 +0300

    installed material core and lab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { Header } from "./components/Header/Header";
+import { List } from "./components/List/List";
+import { Map } from "./components/Map/Map";
 import "./App.css";
+import { List } from "@mui/material";
 
 export default function App() {
   return (
