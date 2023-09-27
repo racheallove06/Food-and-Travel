2fcf37c4d9764d5dd255b3e800301e11b2d26513
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-27 14:08:04 +0300

    added the api key

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@mui/icons-material/LocationOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import Rating from "@mui/lab";
 import useStyles from "./Styles";
 
 const Map = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(min-width:600px)");
-
+  const coordinates = { lat: 0, lng: 0 };
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{
+          key: "AIzaSyBFunsUmQ7N12nT29zMLRFg_srdOdtHSUo",
+        }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
